8f69ea35df63ec602866d6427318deabc20aaa3f
Fatih Özer <fatihozer@Fatih-Air.home>
2022-04-01 20:26:48 +0300

    Courses front end update

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from . models import Course
+from .models import Course
+# Register your models here.
+@admin.register(Course)
+class courseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'available')
+    list_filter = ('available',)
+    search_fields = ('name','description')
 
-@admin.site.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def course_list(request):
+    return render(request, 'courses.html')
